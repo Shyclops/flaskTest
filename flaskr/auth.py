2f1f67c263ad551bfb40d36490e9b78ff8d48b5a
Christopher Won <christopher.won@queensu.ca>
2020-04-23 16:38:23 -0400

    added wrapper for argument view that checks if user is logged in (to be used for blogposts)

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -79,3 +79,15 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wrap(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
